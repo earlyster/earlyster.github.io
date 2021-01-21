5e63896beb11d6c761a82707ce31df4a59c907e0
Justin Early <813090+earlyster@users.noreply.github.com>
2021-01-20 16:19:42 -0800

    fixing up google tags

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -53,9 +53,28 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-gtag`,
-      trackingIds: [
-        "G-TZM1QXYB8R", // Google Analytics / GA
-      ]
+      options:{
+        trackingIds: [
+          "G-TZM1QXYB8R", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   // optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   // cookie_expires: 0,
+        // },
+        // // This object is used for configuration specific to this plugin
+        // pluginConfig: {
+        //   // Puts tracking script in the head instead of the body
+        //   head: false,
+        //   // Setting this parameter is also optional
+        //   respectDNT: true,
+        //   // Avoids sending pageview hits from custom paths
+        //   exclude: ["/preview/**"],
+        // },
+      }
+     
     },
     `gatsby-plugin-feed`,
     {
